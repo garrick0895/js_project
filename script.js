ebd606e445a576aea3de30e67968616db2adc7a8
Igor <garick0895@mail.ru>
2020-08-02 21:57:25 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Сколько вам лет', '');
+answers[2] = prompt('Где вы работаете?', '');
+
+console.log(answers);
